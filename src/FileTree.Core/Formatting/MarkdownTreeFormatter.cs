cf8c7228350be48e291af8c6a56ff36eb43826f4
unknown <sanjsoroka@outlook.com>
2026-02-14 16:49:12 +0300

    Created formatting classes and updated FileNode model.

--- src/FileTree.Core/Formatting/MarkdownTreeFormatter.cs
+++ src/FileTree.Core/Formatting/MarkdownTreeFormatter.cs
@@ -1,6 +1,28 @@
-﻿namespace FileTree.Core.Formatting;
+﻿using System.Text;
+using FileTree.Core.Models;
 
-public class MarkdownTreeFormatter
+namespace FileTree.Core.Formatting;
+
+public class MarkdownTreeFormatter : ITreeFormatter
 {
-    
+    public string Format(FileNode root)
+    {
+        var sb = new StringBuilder();
+        WriteNode(root, sb, 0);
+        return sb.ToString();
+    }
+
+    private void WriteNode(FileNode node, StringBuilder sb, int depth)
+    {
+        var indent = new string(' ', depth * 2);
+
+        var name = node.IsDirectory ? $"{node.Name}/" : node.Name;
+
+        sb.AppendLine(indent + name);
+
+        foreach (var child in node.Children)
+        {
+            WriteNode(child, sb, depth + 1);
+        }
+    }
 }
