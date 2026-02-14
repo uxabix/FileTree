c7db772c3d996205c543064c5c95c3bae311728a
unknown <sanjsoroka@outlook.com>
2026-02-14 17:41:50 +0300

    Created basic gitignore parsing

---
+++ src/FileTree.Core/GitIgnore/GitIgnoreRule.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileTree.Core.GitIgnore
+{
+    public class GitIgnoreRule
+    {
+        
+    }
+}
